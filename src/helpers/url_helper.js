95ded470e53dd0908970a9528db981d1d00140c5
BerkayEfeTaner <2003022010@student.beykent.edu.tr>
2024-04-02 00:32:46 +0300

    release(url_helper): change GET_ALL_ARE_MOCK_LIST_URL (test)

--- src/helpers/url_helper.js
+++ src/helpers/url_helper.js
@@ -1,7 +1,7 @@
 //Admin Panel
 export const LOGIN_URL = "/auth/login"
 export const GET_ALL_AREA_LIST_URL ="/area/list"
-export const GET_ALL_AREA_MOCK_LIST_URL ="/area/areamock/areas/all"
+export const GET_ALL_AREA_MOCK_LIST_URL ="localhoost:4000/area/areamock/areas/all"
 export const GET_AREA_DETAIL_URL ="/device/customer-device/area/"
 export const GET_AREA_LINE_CHART_URL ="/area/areamock/"
 export const GET_TOP_PRODUCTION_POWER_URL ="/area/production_power_order"
